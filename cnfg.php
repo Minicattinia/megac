7ed5c05b281a4549f11d85623d05fdcb88f3b635
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2020-04-04 10:52:09 +0700

    Add files via upload

---
+++ cnfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/*Configs By HACK-TO-WIN*/
+
+
+// Add your email //
+$info = 'https://megacash.me/server/backend/credits/setClientRewarded.php?email=xxxxxxxxx&removeCredits=0&which=GPlay';
+
+?>
+
+
+
